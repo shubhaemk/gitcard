9f5318c5e5f6da7b7160a0148248e24ce91315c7
Shubham Kamath <shubham@Shubhams-MacBook-Air.local>
2019-06-27 23:59:20 +0530

    Added image instead of FA

--- src/components/userCard/repos.jsx
+++ src/components/userCard/repos.jsx
@@ -13,36 +13,36 @@ class Repos extends Component{
                     <div className='column is-6'>
                         <div className='has-margin-bottom-10'>                         
                             <span style={{fontFamily : 'Roboto Mono' , fontSize: '15px', color : this.props.card_text, display: 'inline-block', width: '23ch', whiteSpace: 'nowrap', overflow: 'hidden',textOverflow: 'ellipsis'}}>
-                                <i className="fas fa-book has-margin-right-10"></i>{this.props.latest_repos === null ? null : (this.props.latest_repos[0] === undefined ? 'N/A' : this.props.latest_repos[0]['name'])}
+                                <img className="has-margin-right-10" src={'./assets/book-'+this.props.theme+'.svg'} alt='location' height='10px' width='10px'></img>{this.props.latest_repos === null ? null : (this.props.latest_repos[0] === undefined ? 'N/A' : this.props.latest_repos[0]['name'])}
                             </span>
                             <p style={{fontFamily : 'Roboto Mono' , fontSize: '11px', color : this.props.card_subtext}}>
-                                <i className="far fa-dot-circle has-margin-right-5"></i>{this.props.latest_repos === null ? null : (this.props.latest_repos[0] === undefined ? 'N/A' : this.props.latest_repos[0]['language'])}
+                                <img className="has-margin-right-10" src={'./assets/circle-'+this.props.theme+'.svg'} alt='location' height='10px' width='10px'></img>{this.props.latest_repos === null ? null : (this.props.latest_repos[0] === undefined ? 'N/A' : this.props.latest_repos[0]['language'])}
                             </p>
                         </div>
                         <div>
                             <span style={{fontFamily : 'Roboto Mono' , fontSize: '15px', color : this.props.card_text, display: 'inline-block', width: '23ch', whiteSpace: 'nowrap', overflow: 'hidden',textOverflow: 'ellipsis'}}>
-                                <i className="fas fa-book has-margin-right-10"></i>{this.props.latest_repos === null ? null : (this.props.latest_repos[1] === undefined ? 'N/A' : this.props.latest_repos[1]['name'])}
+                                <img className="has-margin-right-10" src={'./assets/book-'+this.props.theme+'.svg'} alt='location' height='10px' width='10px'></img>{this.props.latest_repos === null ? null : (this.props.latest_repos[1] === undefined ? 'N/A' : this.props.latest_repos[1]['name'])}
                             </span>
                             <p style={{fontFamily : 'Roboto Mono' , fontSize: '11px', color : this.props.card_subtext}}>
-                                <i className="far fa-dot-circle has-margin-right-5"></i>{this.props.latest_repos === null ? null : (this.props.latest_repos[1] === undefined ? 'N/A' : this.props.latest_repos[1]['language'])}
+                                <img className="has-margin-right-10" src={'./assets/circle-'+this.props.theme+'.svg'} alt='location' height='10px' width='10px'></img>{this.props.latest_repos === null ? null : (this.props.latest_repos[1] === undefined ? 'N/A' : this.props.latest_repos[1]['language'])}
                             </p>
                         </div>
                     </div>
                     <div className='column is-6'>
                         <div className='has-margin-bottom-10'>
                             <span style={{fontFamily : 'Roboto Mono' , fontSize: '15px', color : this.props.card_text, display: 'inline-block', width: '23ch', whiteSpace: 'nowrap', overflow: 'hidden',textOverflow: 'ellipsis'}}>
-                                <i className="fas fa-book has-margin-right-10"></i>{this.props.latest_repos === null ? null : (this.props.latest_repos[2] === undefined ? 'N/A' : this.props.latest_repos[2]['name'])}
+                                <img className="has-margin-right-10" src={'./assets/book-'+this.props.theme+'.svg'} alt='location' height='10px' width='10px'></img>{this.props.latest_repos === null ? null : (this.props.latest_repos[2] === undefined ? 'N/A' : this.props.latest_repos[2]['name'])}
                             </span>
                             <p style={{fontFamily : 'Roboto Mono' , fontSize: '11px', color : this.props.card_subtext}}>
-                                <i className="far fa-dot-circle has-margin-right-5"></i>{this.props.latest_repos === null ? null : (this.props.latest_repos[2] === undefined ? 'N/A' : this.props.latest_repos[2]['language'])}
+                                <img className="has-margin-right-10" src={'./assets/circle-'+this.props.theme+'.svg'} alt='location' height='10px' width='10px'></img>{this.props.latest_repos === null ? null : (this.props.latest_repos[2] === undefined ? 'N/A' : this.props.latest_repos[2]['language'])}
                             </p>
                         </div>
                         <div>
                             <span style={{fontFamily : 'Roboto Mono' , fontSize: '15px', color : this.props.card_text, display: 'inline-block', width: '23ch', whiteSpace: 'nowrap', overflow: 'hidden',textOverflow: 'ellipsis'}}>
-                                <i className="fas fa-book has-margin-right-10"></i>{this.props.latest_repos === null ? null : (this.props.latest_repos[3] === undefined ? 'N/A' : this.props.latest_repos[3]['name'])}
+                                <img className="has-margin-right-10" src={'./assets/book-'+this.props.theme+'.svg'} alt='location' height='10px' width='10px'></img>{this.props.latest_repos === null ? null : (this.props.latest_repos[3] === undefined ? 'N/A' : this.props.latest_repos[3]['name'])}
                             </span>
                             <p style={{fontFamily : 'Roboto Mono' , fontSize: '11px', color : this.props.card_subtext}}>
-                                <i className="far fa-dot-circle has-margin-right-5"></i>{this.props.latest_repos === null ? null : (this.props.latest_repos[3] === undefined ? 'N/A' : this.props.latest_repos[3]['language'])}
+                                <img className="has-margin-right-10" src={'./assets/circle-'+this.props.theme+'.svg'} alt='location' height='10px' width='10px'></img>{this.props.latest_repos === null ? null : (this.props.latest_repos[3] === undefined ? 'N/A' : this.props.latest_repos[3]['language'])}
                             </p>
                         </div>
                     </div>
@@ -54,6 +54,7 @@ class Repos extends Component{
 
 const mapStateToProps = (state) => {
     return{
+        theme : state.theme,
         profile : state.profile,
         latest_repos : state.latest_repos,
         card_text : state.card_text,
