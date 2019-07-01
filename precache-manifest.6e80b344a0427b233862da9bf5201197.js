7a1656dac17646630a37f065a1434068221f8168
Shubham Kamath <shubham@Shubhams-MacBook-Air.local>
2019-07-01 23:13:13 +0530

    Updates

---
+++ precache-manifest.6e80b344a0427b233862da9bf5201197.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6417f2493bb29e8166504f68103829bf",
+    "url": "/gitcard/index.html"
+  },
+  {
+    "revision": "ce0e6286492c72d4dd5a",
+    "url": "/gitcard/static/css/2.457a6994.chunk.css"
+  },
+  {
+    "revision": "c52ac5f817645144ae6f",
+    "url": "/gitcard/static/css/main.a72ced97.chunk.css"
+  },
+  {
+    "revision": "ce0e6286492c72d4dd5a",
+    "url": "/gitcard/static/js/2.6eb9302c.chunk.js"
+  },
+  {
+    "revision": "c52ac5f817645144ae6f",
+    "url": "/gitcard/static/js/main.0033af1c.chunk.js"
+  },
+  {
+    "revision": "63086d898bcff8c9dc46",
+    "url": "/gitcard/static/js/runtime~main.8b074d3f.js"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/gitcard/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/gitcard/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/gitcard/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/gitcard/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/gitcard/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/gitcard/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/gitcard/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/gitcard/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/gitcard/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/gitcard/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/gitcard/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/gitcard/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/gitcard/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/gitcard/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/gitcard/static/media/fa-solid-900.f14c3b2f.ttf"
+  }
+]);
